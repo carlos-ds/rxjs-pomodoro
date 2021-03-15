2f02a2eef1401d063adfd17865fd51cf70c3775d
Karel De Smet <karel.de.smet@outlook.com>
2021-03-15 22:03:42 +0100

    Make focus and break time adjustable

--- js/index.js
+++ js/index.js
@@ -5,24 +5,26 @@ import { takeUntil, map, takeWhile } from 'rxjs/operators';
 document.addEventListener('DOMContentLoaded', (event) => {
   const timeRemaining = document.getElementById('timer');
 
-  let timer = new Timer('focus', 25 * 60 * 1000);
-  setTimer(timer);
-
   const startButton = document.querySelector('.btn--action-start');
   const stopButton = document.querySelector('.btn--action-stop');
   const focusButton = document.querySelector('.btn--action-focus');
   const breakButton = document.querySelector('.btn--action-break');
-
-  disable(stopButton);
-
   const buttons = [startButton, stopButton, focusButton, breakButton];
 
   const clickStop$ = fromEvent(stopButton, 'click');
   const clickFocus$ = fromEvent(focusButton, 'click');
   const clickBreak$ = fromEvent(breakButton, 'click');
   const clickStart = fromEvent(startButton, 'click');
-
   const stopTimer$ = merge(clickBreak$, clickFocus$, clickStop$);
+
+  const focusTimeInput = document.querySelector("input[name='focus']");
+  const breakTimeInput = document.querySelector("input[name='break']");
+  focusTimeInput.value = '25';
+  breakTimeInput.value = '5';
+
+  let timer = new Timer('focus', getFocusTime() * 60 * 1000);
+  setTimer(timer);
+  disable(stopButton);
 
   const startTimer$ = interval(1000).pipe(
     map((x) => (timer.time -= 1000)),
@@ -53,14 +55,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
   });
 
   focusButton.addEventListener('click', () => {
-    timer = new Timer('focus', 25 * 60 * 1000);
+    timer = new Timer('focus', getFocusTime() * 60 * 1000);
     setTimer(timer);
     enable(buttons);
     disable(focusButton);
   });
 
   breakButton.addEventListener('click', () => {
-    timer = new Timer('break', 5 * 60 * 1000);
+    timer = new Timer('break', getBreakTime() * 60 * 1000);
     setTimer(timer);
     enable(buttons);
     disable(breakButton);
@@ -75,6 +77,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
     timeRemaining.textContent = formatInMinutesAndSeconds(timer.time);
     document.title = `${timer.type} time - ${formatInMinutesAndSeconds(timer.time)}`;
     document.querySelector('.header-text').textContent = timer.type.toUpperCase() + ' time';
+  }
+
+  function getBreakTime() {
+    return parseInt(breakTimeInput.value, 10);
+  }
+
+  function getFocusTime() {
+    return parseInt(focusTimeInput.value, 10);
   }
 
   function disable(button) {
